862cd8c348c76f24837bb049734aef6321aeb961
Tyuiu.BelousovaOD@outlook.com <bbb_od@mail.ru>
2025-01-24 03:31:39 +0500

    Task6

--- Tyuiu.BelousovaOD.Sprint6.Task6.V20.Test/DataServiceTest.cs
+++ Tyuiu.BelousovaOD.Sprint6.Task6.V20.Test/DataServiceTest.cs
@@ -8,29 +8,7 @@ namespace Tyuiu.BelousovaOD.Sprint6.Task6.V20.Test
         [TestMethod]
         public void TestMethod1()
         {
-            string resStr = "";
 
-            using (StreamReader reader = new StreamReader(path))
-            {
-                string line;
-                while ((line = reader.ReadLine()) != null)
-                {
-                    if (line.Contains(str))
-                    {
-                        resStr += " " + line;
-                    }
-                }
-                string[] array = resStr.Split(" ");
-                resStr = "";
-                for (int i = 0; i < array.Length; i++)
-                {
-                    if (i % 2 == 0)
-                    {
-                        resStr += array[i] + " ";
-                    }
-                }
-
-            }
-            return resStr;
         }
+    }
 }
--- Tyuiu.BelousovaOD.Sprint6.Task6.V20/FormMain.cs
+++ Tyuiu.BelousovaOD.Sprint6.Task6.V20/FormMain.cs
@@ -7,28 +7,10 @@ namespace Tyuiu.BelousovaOD.Sprint6.Task6.V20
         {
             InitializeComponent();
         }
-        DataService ds = new DataService();
-        private void button_Imput_Click(object sender, EventArgs e)
-        {
-            openFileDialog1.ShowDialog();
-            string fileName = openFileDialog1.FileName;
-            openFilePath.Text = fileName;
-            textBox_Imput.Text = File.ReadAllText(fileName);
-            button_Go.Enabled = true;
 
-        }
-
-        private void button_Go_Click(object sender, EventArgs e)
+        private void FormMain_Load(object sender, EventArgs e)
         {
-            string str = " ";
-            string path = Convert.ToString(openFilePath.Text);
-            textBox_Res.Text = ds.CollectTextFromFile(path);
-        }
 
-        private void button_Spr_Click(object sender, EventArgs e)
-        {
-            About formAbout = new About();
-            formAbout.ShowDialog();
         }
     }
 }
