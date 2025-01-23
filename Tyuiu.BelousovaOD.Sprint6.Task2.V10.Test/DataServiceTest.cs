1b87ea2730a153de525413d9bcfd79f6fab455f4
Tyuiu.BelousovaOD@outlook.com <bbb_od@mail.ru>
2025-01-24 02:08:14 +0500

    Task2

---
+++ Tyuiu.BelousovaOD.Sprint6.Task2.V10.Test/DataServiceTest.cs
@@ -0,0 +1,15 @@
+﻿using Tyuiu.BelousovaOD.Sprint6.Task2.V10.Lib;
+namespace Tyuiu.BelousovaOD.Sprint6.Task2.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new();
+            double[] doubles = { };
+            CollectionAssert.AreEqual(doubles, ds.GetMassFunction(-5, 5));
+        }
+    }
+}
---
+++ Tyuiu.BelousovaOD.Sprint6.Task2.V10/FormMain.cs
@@ -0,0 +1,30 @@
+﻿using Tyuiu.BelousovaOD.Sprint6.Task2.V10.Lib;
+namespace Tyuiu.BelousovaOD.Sprint6.Task2.V10
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        private void GoButton_RAYu_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                DataService ds = new();
+                double[] res = ds.GetMassFunction(int.Parse(TextStart_RAYu.Text), int.Parse(TextStop_RAYu.Text));
+                for (int i = 0; i < res.Length; i++)
+                {
+                    dataGridView1.Rows.Add(int.Parse(TextStart_RAYu.Text) + i, res[i]);
+                }
+            }
+            catch { MessageBox.Show("Íåâåðíûå äàííûå", "Îøèáêà"); }
+        }
+
+        private void HelpButton_RAYu_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Âûïîëíèë Ðîãîâ Àëåêñàíäð Þðüåâè÷, ÏÊÒá-24-1", "Ñîîáùåíèå");
+        }
+    }
+}
